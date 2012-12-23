7190d284a1dbcf8258a6ebd63d00b72d121b84e7
Mario Jakopec <mario@faveo.hr>
2012-12-23 21:58:00 +0100

    Update config/initializers/secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MyRails::Application.config.secret_token = '6114032618d22ada76249257e07955eb6b9baaebc7c8694d3dea3c1686f26f5af0e951af8e992c0584e958e5659180597a85799c7532317a4ae85652f68a4a92'
